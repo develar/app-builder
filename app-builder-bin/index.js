2e5551dcb99c9667936ef60179f5f3d2bae98267
Dennis Ameling <dennis@dennisameling.com>
2021-02-22 18:10:22 +0100

    Use 32-bit (ia32) app-builder.exe on Windows ARM64

--- app-builder-bin/index.js
+++ app-builder-bin/index.js
@@ -11,6 +11,15 @@ function getPath() {
   if (platform === "darwin") {
     return path.join(__dirname, "mac", "app-builder")
   }
+  else if (platform === "win32" && process.arch == "arm64") {
+    /**
+     * Golang isn't available for Windows ARM64 yet, so an ARM64 binary
+     * can't be built yet. However, Windows ARM64 does support ia32
+     * emulation, so we can leverage the existing executable for ia32.
+     * https://github.com/golang/go/issues/36439
+     */
+    return path.join(__dirname, "win", "ia32", "app-builder.exe")
+  }
   else if (platform === "win32") {
     return path.join(__dirname, "win", process.arch, "app-builder.exe")
   }
