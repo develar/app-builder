695647bcfd6afd4af745dbe2709e1d31475a91d9
Mike Maietta <mmaietta@users.noreply.github.com>
2022-03-03 00:21:20 -0800

    Update appImage.go (#77)
    See: https://github.com/electron-userland/electron-builder/issues/6678

--- pkg/package-format/appimage/appImage.go
+++ pkg/package-format/appimage/appImage.go
@@ -43,7 +43,7 @@ func ConfigureCommand(app *kingpin.Application) {
 		template: command.Flag("template", "The template file.").String(),
 		license:  command.Flag("license", "The license file.").String(),
 
-		compression: command.Flag("compression", "The compression.").Enum("xz", "gzip"),
+		compression: command.Flag("compression", "The compression.").Default("zstd").Enum("xz", "lzo", "zstd"),
 	}
 
 	configuration := command.Flag("configuration", "").Required().String()
