be4e7ec9c438e7f803c120a66148950ba294dae5
beyondkmp <beyondkmp@gmail.com>
2024-04-30 01:36:24 +0800

    feat: add flatten option to `node-dep-tree` for rendering dependency conflicts in a different manner (#116)

--- pkg/node-modules/cli_test.go
+++ pkg/node-modules/cli_test.go
@@ -22,6 +22,27 @@ type NodeTreeItem struct {
 	Deps []NodeTreeDepItem `json:"deps"`
 }
 
+func nodeDepPath(t *testing.T, dir string) {
+	g := NewGomegaWithT(t)
+	rootPath := fs.FindParentWithFile(Dirname(), "go.mod")
+	cmd := exec.Command("go", "run", path.Join(rootPath, "main.go"), "node-dep-tree", "--flatten", "--dir", dir)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	g.Expect(err).NotTo(HaveOccurred())
+	var j []NodeTreeItem
+	json.Unmarshal(output, &j)
+	r := lo.FlatMap(j, func(it NodeTreeItem, i int) []string {
+		return lo.Map(it.Deps, func(it NodeTreeDepItem, i int) string {
+			return it.Name
+		})
+	})
+	g.Expect(r).To(ConsistOf([]string{
+		"react", "js-tokens", "loose-envify",
+	}))
+}
+
 func nodeDepTree(t *testing.T, dir string) {
 	g := NewGomegaWithT(t)
 	rootPath := fs.FindParentWithFile(Dirname(), "go.mod")
@@ -44,6 +65,11 @@ func nodeDepTree(t *testing.T, dir string) {
 }
 
 func TestNodeDepTreeCmd(t *testing.T) {
+	nodeDepTree(t, path.Join(Dirname(), "npm-demo"))
+	nodeDepTree(t, path.Join(Dirname(), "pnpm-demo"))
+}
+
+func TestNodeDepPathCmd(t *testing.T) {
 	nodeDepTree(t, path.Join(Dirname(), "npm-demo"))
 	nodeDepTree(t, path.Join(Dirname(), "pnpm-demo"))
 }
