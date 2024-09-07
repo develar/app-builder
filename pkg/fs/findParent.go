bdbed41fa0b98135cd48ec0c0577b433a28ed23f
beyondkmp <beyondkmkp@gmail.com>
2024-09-08 07:59:14 +0800

    fix parent issus

--- pkg/fs/findParent.go
+++ pkg/fs/findParent.go
@@ -1,23 +1,31 @@
 package fs
 
 import (
-	"os"
-	"path"
-	"path/filepath"
+        "os"
+        "path"
+        "path/filepath"
 )
 
 func pathExists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil
+        info, err := os.Stat(path)
+        if err != nil {
+                return false
+        }
+        if !info.IsDir() {
+                return false
+        }
+        packageJsonPath := filepath.Join(path, "package.json")
+        _, err = os.Stat(packageJsonPath)
+        return err == nil
 }
 
 func FindParentWithFile(cwd string, file string) string {
-	if pathExists(path.Join(cwd, file)) {
-		return cwd
-	}
-	parent := filepath.Dir(cwd)
-	if parent == cwd {
-		return ""
-	}
-	return FindParentWithFile(parent, file)
+        if pathExists(path.Join(cwd, file)) {
+                return cwd
+        }
+        parent := filepath.Dir(cwd)
+        if parent == cwd {
+                return ""
+        }
+        return FindParentWithFile(parent, file)
 }
